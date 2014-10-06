44e8ff0fc418fc67e4365438d610f28bc6b435c8
motte <michaelotte1@gmail.com>
2014-10-06 01:06:10 -0700

    [MO] Initial Push
    - Has “Sunshine” Launcher Icon

---
+++ ButterMellow/app/src/androidTest/java/com/michaelotte/mlo/buttermellow/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.michaelotte.mlo.buttermellow;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
