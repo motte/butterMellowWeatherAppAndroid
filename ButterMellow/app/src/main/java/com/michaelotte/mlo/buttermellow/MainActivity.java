0b0cb892441211b3776751a2685bbc300e1dab14
motte <michaelotte1@gmail.com>
2014-10-06 01:16:47 -0700

    [MO] MainActivity with Dummy Weather Forecast Data
    - Array list of dummy weather forecast data for main activity
    - New list_item_forecast TextView layout for main activity

--- ButterMellow/app/src/main/java/com/michaelotte/mlo/buttermellow/MainActivity.java
+++ ButterMellow/app/src/main/java/com/michaelotte/mlo/buttermellow/MainActivity.java
@@ -4,15 +4,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.os.Build;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 
+/**
+ * Runs when app is launched
+ */
 public class MainActivity extends ActionBarActivity {
 
     @Override
@@ -51,16 +60,57 @@ public class MainActivity extends ActionBarActivity {
 
     /**
      * A placeholder fragment containing a simple view.
+     * fragment is a modular container
      */
     public static class PlaceholderFragment extends Fragment {
+
+        private ArrayAdapter<String> dayForecastAdapter;
 
         public PlaceholderFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            String[] forecastArray = {
+                    "Today - Sunny - 88/63",
+                    "Tomorrow - NOOOOO stay inside - 0/-10",
+                    "Tuesday - Sunny - 88/65",
+                    "Wednesday - Warm - 76/70",
+                    "Thursday - Cool - 70/60",
+                    "Friday - Cold - 32/0",
+                    "Saturday - Brrrr - 0/-30",
+                    "Sunday - Cool - 70/60",
+                    "Monday - Cold - 32/0",
+                    "Tuesday - Brrrr - 0/-30",
+            };
+
+            List<String> weekForecast = new ArrayList<String>(
+                    Arrays.asList(forecastArray)
+            );
+
+            /**
+             * The ArrayAdapter does all the control of the view(s)
+             * This one binds a string and gets all the data associated with it.
+             * It also considers the orders of the views
+             * This will pass along the view information to the bound view itself
+             */
+            dayForecastAdapter =
+                    new ArrayAdapter<String>(
+                            // The current context (this fragment's parent activity that are global)
+                            getActivity(),
+                            R.layout.list_item_forecast,
+                            R.id.list_item_forecast_textview,
+                            // Forecast data in an array
+                            weekForecast);
+
+            ListView listView = (ListView) rootView.findViewById(
+                    R.id.listview_forecast);
+            listView.setAdapter(dayForecastAdapter);
+
+
             return rootView;
         }
     }
